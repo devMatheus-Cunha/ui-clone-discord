45d4a905713f27517e4a1d19ab458cdf1304be0c
Matheus Gomes <gomesprogramacao@gmail.com>
2021-08-13 08:38:24 -0300

    feat: structure of component ChannelData

---
+++ src/components/ChannelData/index.tsx
@@ -0,0 +1,52 @@
+import React, { useRef, useEffect } from 'react';
+
+import ChannelMessage, { Mention } from '../ChannelMessage';
+
+import { Container, Messages, InputWrapper, Input, InputIcon } from './styles';
+
+const ChannelData: React.FC = () => {
+  const messagesRef = useRef() as React.MutableRefObject<HTMLDivElement>;
+
+  useEffect(() => {
+    const div = messagesRef.current;
+
+    if (div) {
+      div.scrollTop = div.scrollHeight;
+    }
+  }, [messagesRef]);
+
+  return (
+    <Container>
+      <Messages ref={messagesRef}>
+        {Array.from(Array(15).keys()).map((n) => (
+          <ChannelMessage
+            key={n}
+            author="Matheus Gomes"
+            date="21/06/2020"
+            content="Hoje é o meu aniversário!"
+          />
+        ))}
+
+        <ChannelMessage
+          author="Diego Fernandes"
+          date="21/06/2020"
+          content={
+            <>
+              <Mention>@mthgomes</Mention>, me carrega r6 dnv
+              por favor?
+            </>
+          }
+          hasMention
+          isBot
+        />
+      </Messages>
+
+      <InputWrapper>
+        <Input type="text" placeholder="Conversarem #chat-livre" />
+        <InputIcon />
+      </InputWrapper>
+    </Container>
+  );
+};
+
+export default ChannelData;
